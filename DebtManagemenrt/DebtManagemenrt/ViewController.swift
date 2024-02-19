f9bc63e048d2fb27aa09e3cd6e01382e6e168bcd
naveenvenna70 <naveenvenna70@gmail.com>
2024-02-18 18:44:07 -0800

    Created Project

---
+++ DebtManagemenrt/DebtManagemenrt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DebtManagemenrt
+//
+//  Created by Naveen Reddy Venna on 2/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
