9ce3175fbfce2d2146aefee401f3c42b39d5477b
venkateswararo <venedu3387@gmail.com>
2024-02-20 19:39:41 -0600

    added Lottie Animation

--- DebtManagemenrt/DebtManagemenrt/DebtVC.swift
+++ DebtManagemenrt/DebtManagemenrt/DebtVC.swift
@@ -6,9 +6,21 @@
 //
 
 import UIKit
+import Lottie
 
 class DebtVC: UIViewController {
-
+    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
+        didSet{
+            LaunchLAV.animation = .named("Animation")
+            LaunchLAV.alpha=1
+            LaunchLAV.play(){[weak self] _ in
+                        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1, delay: 0.1, options: [.curveEaseIn]){
+                            self!.LaunchLAV.alpha = 0
+                        }
+                    }
+                }
+            }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
