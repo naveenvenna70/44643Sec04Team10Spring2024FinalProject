6820427952d5e5c9f6e7cae2204ab0abacd4007c
Navyasree Sriperumbudoor <s567092@nwmissouri.edu>
2024-02-22 22:43:19 -0800

    Outlets have been created for the Debt signup VC!

---
+++ DebtManagemenrt/DebtManagemenrt/DebtsignupVC.swift
@@ -0,0 +1,128 @@
+//
+//  DebtsignupVC.swift
+//  DebtManagemenrt
+//
+//  Created by Navyasree Sriperumbudoor on 2/22/24.
+//
+
+import UIKit
+
+class DebtsignupVC: UIViewController {
+    
+    
+    
+    @IBOutlet weak var BigContainerSV: UIStackView!
+    
+    
+    @IBOutlet weak var DebtTV: UIImageView!
+    
+    
+    @IBOutlet weak var FirstnameSV: UIStackView!
+    
+    
+    @IBOutlet weak var FirstNameLBL: UILabel!
+    
+    @IBOutlet weak var FirstNameTF: UITextField!
+    
+    
+    @IBOutlet weak var SecondnameSV: UIStackView!
+    
+    
+    @IBOutlet weak var LastNameLBL: UILabel!
+    
+    
+    @IBOutlet weak var LastNameTF: UITextField!
+    
+    @IBOutlet weak var EmailSV: UIStackView!
+    
+    
+    @IBOutlet weak var EmailIDLBL: UILabel!
+    
+    
+    @IBOutlet weak var EmailIDTF: UITextField!
+    
+    
+    @IBOutlet weak var UsernameSV: UIStackView!
+    
+    
+    @IBOutlet weak var UserNameLBL: UILabel!
+    
+    @IBOutlet weak var UserNameTF: UITextField!
+    
+    
+    @IBOutlet weak var NumberSV: UIStackView!
+    
+    
+    @IBOutlet weak var PhonenumberLBL: UILabel!
+    
+    @IBOutlet weak var CountrycodeTF: UITextField!
+    
+    @IBOutlet weak var NumberTF: UITextField!
+    
+    
+    @IBOutlet weak var PasswordSV: UIStackView!
+    
+    
+    @IBOutlet weak var PasswordLBL: UILabel!
+    
+    
+    @IBOutlet weak var PasswordTF: UITextField!
+    
+    
+    @IBOutlet weak var ConfirmSV: UIStackView!
+    
+    @IBOutlet weak var ConfirmUserNameLBL: UILabel!
+    
+    @IBOutlet weak var ConfirmUserNameTF: UITextField!
+    
+    
+    @IBOutlet weak var SignupBTN: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
