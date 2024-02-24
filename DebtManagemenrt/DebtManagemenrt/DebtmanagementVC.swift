d1bb0875bff8e2243c53c97e96e6cdb6eb57e9e6
Navyasree Sriperumbudoor <s567092@nwmissouri.edu>
2024-02-23 16:34:25 -0800

    Navya sree : third commit done! Outlets have been created for Debt ManagementVC!

--- DebtManagemenrt/DebtManagemenrt/DebtmanagementVC.swift
+++ DebtManagemenrt/DebtManagemenrt/DebtmanagementVC.swift
@@ -8,12 +8,103 @@
 import UIKit
 
 class DebtmanagementVC: UIViewController {
+    
+    
+    @IBOutlet weak var BigContainerSV: UIStackView!
+    
+    
+    @IBOutlet weak var DebtManagementIV: UIImageView!
+    
+    
+    @IBOutlet weak var ButtonsSV: UIStackView!
+    
+    @IBOutlet weak var AboutUsBTN: UIButton!
+    
+    @IBOutlet weak var AdvicesBTN: UIButton!
+    
+    @IBOutlet weak var ReviewsBTN: UIButton!
+    
+    
+    @IBOutlet weak var HelpBTN: UIButton!
+    
+    @IBOutlet weak var DebtSV: UIStackView!
+    
+    
+    @IBOutlet weak var DebtButtonsSV: UIStackView!
+    
+    
+    @IBOutlet weak var DebtTrackBTN: UIButton!
+    
+    
+    @IBOutlet weak var LoanEstimationBTN: UIButton!
+    
+    
+    @IBOutlet weak var ScheduleSV: UIStackView!
+    
+    
+    @IBOutlet weak var PaymentSchedBTN: UIButton!
+    
+    @IBOutlet weak var CalenderBTN: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 
     /*
