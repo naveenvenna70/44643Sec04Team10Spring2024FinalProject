4572e95754592ba5ead0e3d0977140ddb2aa6aaf
venkateswararo <venedu3387@gmail.com>
2024-03-08 13:15:47 -0600

    Commiting outlet connections again for PaymentScheduleVC

--- DebtManagemenrt/DebtManagemenrt/PaymentScheduleVC.swift
+++ DebtManagemenrt/DebtManagemenrt/PaymentScheduleVC.swift
@@ -2,13 +2,27 @@
 //  PaymentScheduleVC.swift
 //  DebtManagemenrt
 //
-//  Created by venkat on 07/03/24.
+//  Created by venkat on 08/03/24.
 //
 
 import UIKit
 
 class PaymentScheduleVC: UIViewController {
-
+    @IBOutlet weak var LoanAmountTF: UITextField!
+    
+    @IBOutlet weak var paymentStartDateTF: UITextField!
+    
+    @IBOutlet weak var EstimatedLoanDeadlineTF: UITextField!
+    
+    @IBOutlet weak var EstimatedmonthlyDueDateTF: UITextField!
+    
+    @IBOutlet weak var IfNotPaidEstimatedAmountToBePiadTF: UITextField!
+    
+    @IBOutlet weak var EstimatedCreditScoreTF: UITextField!
+    
+    @IBOutlet weak var CompleteTransactionsTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
