6d5c80a32af2b4a188e0fc31bcac3bf9ab05c05c
naveenvenna70 <naveenvenna70@gmail.com>
2024-04-15 18:37:49 -0700

    Naveen :Added SignUp page Functionality

--- DebtManagemenrt/DebtManagemenrt/DebtsignupVC.swift
+++ DebtManagemenrt/DebtManagemenrt/DebtsignupVC.swift
@@ -8,12 +8,7 @@
 import UIKit
 
 class DebtsignupVC: UIViewController {
-    
-    
 
-    
-    
-    @IBOutlet weak var DebtTV: UIImageView!
     
     
     @IBOutlet weak var FirstnameSV: UIStackView!
@@ -69,56 +64,142 @@ class DebtsignupVC: UIViewController {
 
     @IBOutlet weak var confirmUserNameLBL: UILabel!
     
-    @IBOutlet weak var confirmUserNameTF: UITextField!
+    @IBOutlet weak var confirmPasswordTF: UITextField!
     
     
     @IBOutlet weak var SignupBTN: UIButton!
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    @IBAction func SignUpBTN(_ sender: UIButton) {
+        guard let email = EmailIDTF.text,
+               let phonenumber = NumberTF.text,
+               let password = PasswordTF.text,
+               let firstName = FirstNameTF.text,
+               let lastName = LastNameTF.text,
+               let confirmPassword = confirmPasswordTF.text else {
+             openAlert(title: "Alert", message: "Please validate your details", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             print("Please check your details")
+             return
+         }
+         
+         if firstName.isEmpty {
+             openAlert(title: "Alert", message: "Please enter firstname", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             return
+         }
+         
+         if lastName.isEmpty {
+             openAlert(title: "Alert", message: "Please enter lastname", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             return
+         }
+         
+         if let emailError = inValidEmail(email) {
+             openAlert(title: "Alert", message: emailError, alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             return
+         }
+         
+         if let phoneError = isValidPhoneNumber(phonenumber) {
+             openAlert(title: "Alert", message: phoneError, alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             return
+         }
+         
+         if let passwordError = inValidPassword(password) {
+             openAlert(title: "Alert", message: passwordError, alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             print("Password is not valid")
+             return
+         }
+         
+         if confirmPassword.isEmpty {
+             openAlert(title: "Alert", message: "Please confirm password", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             print("Please confirm password")
+             return
+         }
+         
+         if password != confirmPassword {
+             openAlert(title: "Alert", message: "Passwords don't match", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+             print("Passwords don't match")
+             return
+         }
+         
+         openAlert(title: "Alert", message: "Account created", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+         FirstNameTF.text = ""
+         LastNameTF.text = ""
+         EmailIDTF.text = ""
+         NumberTF.text = ""
+         PasswordTF.text = ""
+         confirmPasswordTF.text = ""
+     }
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view.
+     }
+
+     func inValidEmail(_ value: String) -> String? {
+         let emailRegularExp = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+         let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegularExp)
+         if !predicate.evaluate(with: value ) {
+             return "Please enter valid email address"
+         }
+         return nil
+     }
+
+     func inValidPassword(_ value: String) -> String? {
+         if value.count < 8 {
+             return "Password must be at least 8 characters long"
+         }
+         if !containsDigit(value) {
+             return "Password must contain at least 1 digit"
+         }
+         if !containsLowerCase(value) {
+             return "Password must contain at least 1 lowercase character"
+         }
+         if !containsUpperCase(value) {
+             return "Password must contain at least 1 uppercase character"
+         }
+         return nil
+     }
+
+     func containsDigit(_ value: String) -> Bool {
+         let psdExpression = ".*[0-9]+.*"
+         let psdpredicate = NSPredicate(format: "SELF MATCHES %@", psdExpression)
+         return psdpredicate.evaluate(with: value)
+     }
+
+     func containsLowerCase(_ value: String) -> Bool {
+         let psdExpression = ".*[a-z]+.*"
+         let psdpredicate = NSPredicate(format: "SELF MATCHES %@", psdExpression)
+         return psdpredicate.evaluate(with: value)
+     }
+
+     func containsUpperCase(_ value: String) -> Bool {
+         let psdExpression = ".*[A-Z]+.*"
+         let psdpredicate = NSPredicate(format: "SELF MATCHES %@", psdExpression)
+         return psdpredicate.evaluate(with: value)
+     }
+
+     func isValidPhoneNumber(_ value: String) -> String? {
+         let phoneRegex = #"^\d{10}$"#
+         let predicate = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+         if !predicate.evaluate(with: value) {
+             return "Please enter valid 10 digits phone number"
+         }
+         return nil
+     }
+
+     public func openAlert(title: String,
+                           message: String,
+                           alertStyle:UIAlertController.Style,
+                           actionTitles:[String],
+                           actionStyles:[UIAlertAction.Style],
+                           actions: [((UIAlertAction) -> Void)]) {
+         let alertController = UIAlertController(title: title, message: message, preferredStyle: alertStyle)
+         for (index, indexTitle) in actionTitles.enumerated() {
+             let action = UIAlertAction(title: indexTitle, style: actionStyles[index], handler: actions[index])
+             alertController.addAction(action)
+         }
+         self.present(alertController, animated: true)
+     }
+ }
+           
     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
